290133c1606ba9d8dccd5204387122f6c3397ea6
Jon Healy <jhealy278@gmail.com>
2023-10-12 00:26:10 -0400

    feat!: Bump Dependency Versions
    BREAKING CHANGE: Major Release 1.0.0 - Java 18, bump JUnit, Jococo and commons-io/net. Drop Nexus since it's EOL.

--- src/test/java/com/axlabs/ip2asn2cc/Ip2Asn2CcExcludeFilterPolicyTest.java
+++ src/test/java/com/axlabs/ip2asn2cc/Ip2Asn2CcExcludeFilterPolicyTest.java
@@ -2,28 +2,28 @@ package com.axlabs.ip2asn2cc;
 
 import com.axlabs.ip2asn2cc.exception.RIRNotDownloadedException;
 import com.axlabs.ip2asn2cc.model.FilterPolicy;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Ip2Asn2CcExcludeFilterPolicyTest {
+class Ip2Asn2CcExcludeFilterPolicyTest {
 
     private static Ip2Asn2Cc ip2Asn2Cc;
 
-    @BeforeClass
-    public static void initialize() throws RIRNotDownloadedException {
-        List<String> listCountryCode = new ArrayList<String>();
+    @BeforeEach
+    void initialize() throws RIRNotDownloadedException {
+        List<String> listCountryCode = new ArrayList<>();
         listCountryCode.add("US");
         ip2Asn2Cc = new Ip2Asn2Cc(listCountryCode, FilterPolicy.EXCLUDE_COUNTRY_CODES);
     }
 
     @Test
-    public void testIPv4() {
+    void testIPv4() {
         // it should return false since the Ip2Asn2Cc class was
         // initialized with "US" country code and the option to "exclude" such
         // country codes in the check
@@ -33,13 +33,13 @@ public class Ip2Asn2CcExcludeFilterPolicyTest {
     }
 
     @Test
-    public void testIPv6() {
+    void testIPv6() {
         assertFalse(ip2Asn2Cc.checkIP("2600:1f18:1f:db01:11af:58af:ae11:f645"));
         assertTrue(ip2Asn2Cc.checkIP("2001:1620:2777:23::2"));
     }
 
     @Test
-    public void testASN() {
+    void testASN() {
         assertFalse(ip2Asn2Cc.checkASN("3356"));
         assertTrue(ip2Asn2Cc.checkASN("13030"));
     }
--- src/test/java/com/axlabs/ip2asn2cc/Ip2Asn2CcIncludeFilterPolicyTest.java
+++ src/test/java/com/axlabs/ip2asn2cc/Ip2Asn2CcIncludeFilterPolicyTest.java
@@ -4,54 +4,56 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import com.axlabs.ip2asn2cc.exception.RIRNotDownloadedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Ip2Asn2CcIncludeFilterPolicyTest {
+class Ip2Asn2CcIncludeFilterPolicyTest {
 
     private static Ip2Asn2Cc ip2Asn2Cc;
 
-    @BeforeClass
-    public static void initialize() throws RIRNotDownloadedException {
+    @BeforeEach
+    void initialize() throws RIRNotDownloadedException {
         List<String> listCountryCode = new ArrayList<>();
         listCountryCode.add("US");
         ip2Asn2Cc = new Ip2Asn2Cc(listCountryCode);
     }
-    
+
     @Test
-    public void getCountryNameTest() throws RIRNotDownloadedException {
+    void getCountryNameTest() throws RIRNotDownloadedException {
         ip2Asn2Cc = new Ip2Asn2Cc(Collections.singletonList("US"));
         System.out.println("Localhost Country: " + ip2Asn2Cc.getRIRCountryCode("127.0.0.1")); //Is hard coded to US
-        
+
         String cc = ip2Asn2Cc.getRIRCountryCode("8.8.8.8");
         boolean status = cc.equals("US");
         System.out.println("Google DNS Country: " + cc); //Should be US
         System.out.println("Test Status: " + (status ? "PASSED" : "FAILED"));
+
+        assertTrue(status);
     }
 
     @Test
-    public void testIPv4() {
+    void testIPv4() {
         // it should return true since the Ip2Asn2Cc class was
         // initialized with "US" country code and the option to "include" such
         // country codes in the check
         assertTrue(ip2Asn2Cc.checkIP("8.8.8.8"));
+
         // It should return false, since it's an IP address from Switzerland
         assertFalse(ip2Asn2Cc.checkIP("77.109.144.219"));
     }
 
     @Test
-    public void testIPv6() {
+    void testIPv6() {
         assertTrue(ip2Asn2Cc.checkIP("2600:1f18:1f:db01:11af:58af:ae11:f645"));
         assertFalse(ip2Asn2Cc.checkIP("2001:1620:2777:23::2"));
     }
 
     @Test
-    public void testASN() {
+    void testASN() {
         assertTrue(ip2Asn2Cc.checkASN("3356"));
         assertFalse(ip2Asn2Cc.checkASN("13030"));
     }
