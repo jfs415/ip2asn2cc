9597f630d3d9913395da1f5659bc5f8afe049937
Jon Healy <jhealy278@gmail.com>
2023-10-13 00:32:05 -0400

    feat: Convert Immutable Classes To Records

--- src/main/java/com/axlabs/ip2asn2cc/model/Ip2Asn2CcEntry.java
+++ src/main/java/com/axlabs/ip2asn2cc/model/Ip2Asn2CcEntry.java
@@ -1,50 +1,6 @@
 package com.axlabs.ip2asn2cc.model;
 
-public class Ip2Asn2CcEntry {
-
-    private final String registry;
-
-    private final String inetFamily;
-
-    private final String address;
-
-    private final String countryCode;
-
-    private final int addresses;
-
-    private final String date;
-
-    public Ip2Asn2CcEntry(String registry, String inetFamily, String address, String countryCode, int addresses, String date) {
-        this.registry = registry;
-        this.inetFamily = inetFamily;
-        this.address = address;
-        this.countryCode = countryCode;
-        this.addresses = addresses;
-        this.date = date;
-    }
-
-    public String getRegistry() {
-        return registry;
-    }
-
-    public String getInetFamily() {
-        return inetFamily;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public int getAddresses() {
-        return addresses;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
+public record Ip2Asn2CcEntry(String registry, String inetFamily, String address, String countryCode, int addresses,
+                             String date) {
 
 }
